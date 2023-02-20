cde5cba298ed51e7de1911babc5b8fc3dd06192b
Migle Urbonaite <91160873+MistyDoe@users.noreply.github.com>
2023-02-20 14:17:29 +0100

    Added FieldTile script

--- FarmingSim/Assets/Scripts/FieldTile.cs
+++ FarmingSim/Assets/Scripts/FieldTile.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class FieldTile : MonoBehaviour
 {
-	//private Crop currCrop;
+	private Crop currCrop;
 
 	public GameObject cropPreFab;
 
@@ -21,6 +21,64 @@ public class FieldTile : MonoBehaviour
 
 	public void Interact()
 	{
-		Debug.Log("Interacted!");
+		if (!tilled)
+		{
+			Till();
+		}
+		else if (!HasCrop() && GameManager.instance.CanPlantCrop())
+		{
+			PlantNewCrop(GameManager.instance.selectedCropToPlant);
+		}
+		else if (HasCrop() && currCrop.CanHarvest())
+		{
+			currCrop.Harvest();
+		}
+		else
+		{
+			currCrop.Water();
+		}
+	}
+
+	public void PlantNewCrop(CropData crop)
+	{
+		if (!tilled)
+			return;
+		currCrop = Instantiate(cropPreFab, transform).GetComponent<Crop>();
+		currCrop.Plant(crop);
+
+		GameManager.instance.onNewDay += OnNewDay;
+	}
+	void Till()
+	{
+		tilled = true;
+		sr.sprite = tilledSprite;
+	}
+	void Water()
+	{
+		sr.sprite = wateredTilledSprite;
+		if (HasCrop())
+		{
+			currCrop.Water();
+		}
+	}
+	void OnNewDay()
+	{
+		if (currCrop == null)
+		{
+			tilled = false;
+			sr.sprite = grassSprite;
+
+			GameManager.instance.onNewDay -= OnNewDay;
+		}
+		else if (currCrop != null)
+		{
+			sr.sprite = tilledSprite;
+			currCrop.NewDayCheck();
+		}
+	}
+
+	bool HasCrop()
+	{
+		return currCrop != null;
 	}
 }
