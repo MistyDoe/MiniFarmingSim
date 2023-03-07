5c38758e9e613e7da79edaf4760a9834e8e126dd
Migle Urbonaite <91160873+MistyDoe@users.noreply.github.com>
2023-03-07 19:50:13 +0100

    Added map generation and testing scripts

---
+++ FarmingSim/Assets/Scripts/Grid.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+	public int width;
+	public int heigth;
+	private float cellSize;
+	private int[,] gridArray;
+
+
+	public Grid(int width, int height, float cellSize)
+	{
+		this.width = width;
+		this.heigth = height;
+		this.cellSize = cellSize;
+
+		gridArray = new int[width, height];
+
+		Debug.Log(width + " " + height);
+		this.cellSize = cellSize;
+
+		for (int x = 0; x < gridArray.GetLength(0); x++)
+		{
+			for (int y = 0; y < gridArray.GetLength(1); y++)
+			{
+				Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.red, 100f);
+				Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.red, 100f);
+			}
+		}
+	}
+
+	private Vector3 GetWorldPosition(int x, int y)
+	{
+		return new Vector3(x, y) * cellSize;
+	}
+
+}
---
+++ FarmingSim/Assets/Scripts/TestCombatMap.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class TestCombatMap : MonoBehaviour
+{
+	void Start()
+	{
+		Grid grid = new Grid(20, 10, 10f);
+	}
+
+
+}
