32107f60a822b485ab8d2664e40c1bd6845f9d97
Migle Urbonaite <91160873+MistyDoe@users.noreply.github.com>
2023-03-10 22:13:49 +0100

    Added methods to Grid class to function with Pathfinding

--- FarmingSim/Assets/Scripts/Grid.cs
+++ FarmingSim/Assets/Scripts/Grid.cs
@@ -13,20 +13,20 @@ public class Grid<TGridObject>
 	public int width;
 	public int heigth;
 	private float cellSize;
-	private int[,] gridArray;
+	private TGridObject[,] gridArray;
 	private Vector3 originPosition;
 
 
-	public Grid(int width, int height, float cellSize, Vector3 originPosition, Func<Grid<TGridObject>>, int , int, TGridObject createdGridObject)
+	public Grid(int width, int height, float cellSize, Vector3 originPosition, Func<Grid<TGridObject>, int, int, TGridObject> createdGridObject)
 	{
 		this.width = width;
 		this.heigth = height;
 		this.cellSize = cellSize;
 		this.originPosition = originPosition;
 
-		gridArray = new int[width, height];
+		gridArray = new TGridObject[width, height];
 
-		Debug.Log(width + " " + height);
+		Debug.Log(width + ", " + height);
 		this.cellSize = cellSize;
 
 		for (int x = 0; x < gridArray.GetLength(0); x++)
@@ -46,14 +46,48 @@ public class Grid<TGridObject>
 	{
 		return new Vector3(x, y) * cellSize + originPosition;
 	}
-	public void Setvalue(int x, int y, int value)
+	public void GetXY(Vector3 worldPosition, out int x, out int y)
+	{
+		x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+		y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+	}
+	public void SetGridObject(int x, int y, TGridObject value)
 	{
 		if (x >= 0 && y >= 0 && x < width && y < heigth)
 		{
 			gridArray[x, y] = value;
 			if (OnGridObjectChange != null) OnGridObjectChange(this, new OnGridObjectChangeEventArgs { x = x, y = y });
-
 		}
+	}
+	public void TriggerGridObjectChanged(int x, int y)
+	{
+		if (OnGridObjectChange != null) OnGridObjectChange(this, new OnGridObjectChangeEventArgs { x = x, y = y });
+	}
+	public void SetGridObject(Vector3 worldPosition, TGridObject value)
+	{
+		int x, y;
+		GetXY(worldPosition, out x, out y);
+		SetGridObject(x, y, value);
+	}
+
+	public TGridObject GetGridObject(int x, int y)
+	{
+		if (x >= 0 && y >= 0 && x < width && y < heigth)
+		{
+			return gridArray[x, y];
+		}
+		else
+		{
+			return default(TGridObject);
+		}
+	}
+
+	public TGridObject GetGridObject(Vector3 worldPosition)
+	{
+		int x, y;
+		GetXY(worldPosition, out x, out y);
+
+		return GetGridObject(x, y);
 	}
 
 }
